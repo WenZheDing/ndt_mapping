5735ee03de127886b84d65bace261ce22ee71730
dingwenzhe <603053103@qq.com>
2022-11-16 20:57:32 +0800

    add filter and fix heading error

--- src/local/src/localmapping.cpp
+++ src/local/src/localmapping.cpp
@@ -3,13 +3,14 @@
 #include <vector>
 #include <sensor_msgs/PointCloud2.h>
 #include <nav_msgs/OccupancyGrid.h>
-#include <geometry_msgs/Point.h>
+#include <geometry_msgs/PoseStamped.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <Eigen/Dense>
+#include <pcl/filters/voxel_grid.h>
 #include <pclomp/ndt_omp.h>
-
+#include "tic_toc.h"
 
 
 using namespace std;
@@ -25,6 +26,8 @@ d_resolution = 0.5, d_height_diff = 0.2, i_max_thresh = 2, i_mid_thresh = 2, i_m
 vector<Eigen::Quaterniond> map_q_;
 vector<Eigen::Vector3d> map_t_;
 vector<double> map_time_;
+bool init;
+double init_time;
 #define MAX_OGM std::numeric_limits<float>::max()
 #define MIN_OGM -std::numeric_limits<float>::max()
 
@@ -142,6 +145,9 @@ void createLocalMap(const pcl::PointCloud<pcl::PointXYZI>::Ptr merged_cloud)
 
 void CallbackLidar(const sensor_msgs::PointCloud2::ConstPtr pcloud)
 {
+    double coff = -1;
+    pcl::VoxelGrid<pcl::PointXYZI> downSizeFilter;
+    downSizeFilter.setLeafSize(0.5, 0.5, 0.5); 
     if(!pose_received) return;
     if (pcloud != NULL)
     {
@@ -150,11 +156,36 @@ void CallbackLidar(const sensor_msgs::PointCloud2::ConstPtr pcloud)
         pcl::fromROSMsg(*pcloud, *plidar);
         CloudFrames.push_back(plidar); 
     }
-    Eigen::Quaterniond tmp_q;
+    Eigen::Quaterniond q, tmp_q;
     Eigen::Matrix3d tmp_R;
-    Eigen::Vector3d tmp_t;
-    pointcloud_q.push_back(last_q);
-    pointcloud_t.push_back(last_t);
+    Eigen::Vector3d t, tmp_t;
+    int index_before = 1, index_after = 1;
+    for(auto iter = map_time_.begin(); iter != map_time_.end(); iter++)
+    {
+        if (pcloud->header.stamp.toSec() >= (*iter) && pcloud->header.stamp.toSec() <= *(iter+1))
+        {
+            index_before = static_cast<int>(iter - map_time_.begin());
+            index_after = index_before + 1;
+            coff = (pcloud->header.stamp.toSec() - map_time_[index_before]) / (map_time_[index_after] - map_time_[index_before]);
+            break;
+        }
+    }
+    if (coff < 0 || coff > 1)
+    {   
+        printf("index_before : %d index_after : %d  map_time_.size(): %d \n", index_before, index_after, map_time_.size());
+        q = map_q_.back();
+        t = map_t_.back(); 
+    }
+    else
+    {
+        Eigen::Quaterniond q_before = map_q_[index_before];
+        Eigen::Quaterniond q_after = map_q_[index_after];
+        q = q_before.slerp(coff, q_after);
+        t = coff * (map_t_[index_after] - map_t_[index_before]) + map_t_[index_before];
+    }
+
+    pointcloud_q.push_back(q);
+    pointcloud_t.push_back(t);
 
     pcl::PointCloud<pcl::PointXYZI>::Ptr tmp(new pcl::PointCloud<pcl::PointXYZI>());
     //ndt
@@ -163,12 +194,13 @@ void CallbackLidar(const sensor_msgs::PointCloud2::ConstPtr pcloud)
     Eigen::Matrix4f trans(Eigen::Matrix4f::Identity());
     //TicToc ndt_time;
     anh_ndt.reset(new pclomp::NormalDistributionsTransform<pcl::PointXYZI, pcl::PointXYZI>());
-    anh_ndt->setResolution(1.0);
-    anh_ndt->setMaximumIterations(50);
+    anh_ndt->setResolution(0.5);
+    anh_ndt->setMaximumIterations(200);
     anh_ndt->setStepSize(0.05);
-    anh_ndt->setTransformationEpsilon(0.0001);
+    anh_ndt->setTransformationEpsilon(1e-6);
+    anh_ndt->setEuclideanFitnessEpsilon(1e-6);
     anh_ndt->setNumThreads(4);
-    anh_ndt->setNeighborhoodSearchMethod(pclomp::DIRECT7);
+    anh_ndt->setNeighborhoodSearchMethod(pclomp::DIRECT26);
     for(int i = 1; i < 20; i += 5)
     {
         palign.reset(new pcl::PointCloud<pcl::PointXYZI>());
@@ -178,9 +210,18 @@ void CallbackLidar(const sensor_msgs::PointCloud2::ConstPtr pcloud)
         tmp_R = pointcloud_q.back().matrix().inverse() * pointcloud_q[id].matrix();
         tmp_q = Eigen::Quaterniond(tmp_R);
         tmp_t = pointcloud_q.back().matrix().inverse() * (pointcloud_t[id] - pointcloud_t.back());
+        Eigen::Vector3d angle = tmp_q.matrix().eulerAngles(2,1,0);
+        printf("tmp_t: %.2lf, %.2lf, %.2lf\n", tmp_t(0), tmp_t(1), tmp_t(2));
+        printf("tmp_q : %.3lf, %.3lf, %.3lf\n", angle(0), angle(1), angle(2));
         Eigen::Translation3d init_translation(tmp_t(0), tmp_t(1), tmp_t(2));
         Eigen::AngleAxisd init_rotation(tmp_q);
         Eigen::Matrix4d init_guess = (init_translation * init_rotation) * Eigen::Matrix4d::Identity();
+        TicToc ndt_time;
+        ndt_time.tic();
+        downSizeFilter.setInputCloud(CloudFrames[id]);
+        downSizeFilter.filter(*CloudFrames[id]);
+        downSizeFilter.setInputCloud(CloudFrames.back());
+        downSizeFilter.filter(*CloudFrames.back());
         anh_ndt->setInputSource(CloudFrames[id]);
         anh_ndt->setInputTarget(CloudFrames.back());
         anh_ndt->align(*palign, init_guess.cast<float>());
@@ -189,25 +230,26 @@ void CallbackLidar(const sensor_msgs::PointCloud2::ConstPtr pcloud)
         bool converged = anh_ndt->hasConverged();
         cout<<"iteration: "<<iteration<<endl;
         cout<<"converged: "<<converged<<endl;
+        cout<<"ndt time: "<< ndt_time.toc() << endl << endl;
         //double ndtTime = ndt_time.toc();
         Eigen::Vector3d t(trans(0, 3), trans(1, 3), trans(2, 3));
         Eigen::Matrix3d R;
         Eigen::Quaterniond q;
         R << trans(0, 0), trans(0, 1), trans(0, 2), trans(1, 0), trans(1, 1), trans(1, 2), trans(2, 0), trans(2, 1), trans(2, 2);
         q=Eigen::Quaterniond(R);
-        pcl::PointCloud<pcl::PointXYZI>::Ptr trans(new pcl::PointCloud<pcl::PointXYZI>());
-        // trans = transformPointCloud(q, t, CloudFrames[id]);
-        trans = transformPointCloud(tmp_q, tmp_t, CloudFrames[id]);
+        pcl::PointCloud<pcl::PointXYZI>::Ptr trans_pcd(new pcl::PointCloud<pcl::PointXYZI>());
+        trans_pcd = transformPointCloud(q, t, CloudFrames[id]);
+        // trans_pcd = transformPointCloud(tmp_q, tmp_t, CloudFrames[id]);
 
-        *tmp += *trans;
+        *tmp += *trans_pcd;
         // debug : save pcd
-        if(CloudFrames.size() == 20)
+        if(CloudFrames.size() == 10)
         {
             string source_pcd = string(ROOT_DIR) + "PCD/" + to_string(id) +".pcd";
             string transformed_source_pcd = string(ROOT_DIR) + "PCD/" + to_string(id) + "_" + to_string(CloudFrames.size() - 1) + "trans.pcd";
             string target_pcd = string(ROOT_DIR) + "PCD/" + to_string(CloudFrames.size() - 1) +".pcd";
             pcl::io::savePCDFileASCII(source_pcd, *CloudFrames[id]);
-            pcl::io::savePCDFileASCII(transformed_source_pcd, *trans);
+            pcl::io::savePCDFileASCII(transformed_source_pcd, *trans_pcd);
             pcl::io::savePCDFileASCII(target_pcd, *CloudFrames.back());
         }
     }
@@ -236,22 +278,25 @@ void CallbackLidar(const sensor_msgs::PointCloud2::ConstPtr pcloud)
 	output.header.frame_id = "conch";
     pub_merged_cloud.publish(output);
     createLocalMap(tmp);
+    cout <<"lidar size():  " << CloudFrames.size() << endl;
 }
 
-void CallbackPose(const geometry_msgs::Point::ConstPtr point)
+void CallbackPose(const geometry_msgs::PoseStamped::ConstPtr pose)
 {
+    if(!init) {init_time = pose->header.stamp.toSec();}
     Eigen::Matrix3d R; 
     Eigen::Quaterniond last_q;
     Eigen::Vector3d last_t;
-    R = Eigen::AngleAxisd(point->z*M_PI/180, Eigen::Vector3d::UnitZ()) * 
+    R = Eigen::AngleAxisd(-pose->pose.position.z*M_PI/180, Eigen::Vector3d::UnitZ()) * 
                       Eigen::AngleAxisd(0, Eigen::Vector3d::UnitY()) * 
                       Eigen::AngleAxisd(0, Eigen::Vector3d::UnitX());
     last_q = Eigen::Quaterniond(R);
-    last_t = Eigen::Vector3d(point->x, point->y, 0);
+    last_t = Eigen::Vector3d(pose->pose.position.x, pose->pose.position.y, 0);
     map_q_.push_back(last_q);
     map_t_.push_back(last_t);
-    // map_time_.push_back(point.stamp.toSec());
+    map_time_.push_back(pose->header.stamp.toSec());
     pose_received = true;
+    cout << "time size(): " <<map_time_.size() << endl;
 }
 
 int main(int argc, char **argv)
@@ -259,8 +304,8 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "localmap");
     ros::NodeHandle nh;
 
-    ros::Subscriber subLidar = nh.subscribe("/lidar_aeb/raw_points", 1, &CallbackLidar);
-    ros::Subscriber subPose = nh.subscribe("/imu_pose", 1, &CallbackPose);
+    ros::Subscriber subLidar = nh.subscribe("/lidar_aeb/raw_points", 1000, &CallbackLidar);
+    ros::Subscriber subPose = nh.subscribe("/imu", 1000, &CallbackPose);
 	pub_map = nh.advertise<nav_msgs::OccupancyGrid>("/occupancy_grid_map", 1);
     pub_merged_cloud = nh.advertise<sensor_msgs::PointCloud2>("/merged_cloud", 1);
 
